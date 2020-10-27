0dbcbbfc90499606505c82ec4804da6b0f25f84a
Alejandro Arteaga <alejow18@gmail.com>
2020-10-26 20:34:11 -0600

    estructura

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeModule } from './Public/pages/home/home.module';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+path: 'home', loadChildren: () => import ('../app/Public/pages/home/home.module').then (m => m.HomeModule),
+
+  },
+  {
+path: 'contact', loadChildren: () => import ('../app/Public/pages/contact/contact.module').then (m => m.ContactModule)
+      },
+      {
+        path: '', pathMatch: 'full', redirectTo : 'home'
+      },
+{
+  path: '**', pathMatch: 'full', redirectTo : 'home'
+}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    {
+    useHash: true,
+    scrollPositionRestoration: 'enabled'
+    }
+    )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
