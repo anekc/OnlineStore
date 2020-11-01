2e89182db92cf0665821cc153b2d6e9170fb217f
Alejandro Arteaga <alejow18@gmail.com>
2020-10-31 23:45:50 -0600

    base de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,38 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeModule } from './Public/pages/home/home.module';
-import { DashboardModule } from './Admin/pages/dashboard/dashboard.module';
-import { UsersModule } from './Admin/pages/users/users.module';
+
 
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () =>
-      import('../app/Public/pages/home/home.module').then((m) => m.HomeModule),
-  },
-  {
-    path: 'contact',
-    loadChildren: () =>
-      import('../app/Public/pages/contact/contact.module').then(
-        (m) => m.ContactModule
-      ),
-  },
-  {
-    path: 'dashboard',
-    loadChildren: () =>
-      import('../app/Admin/pages/dashboard/dashboard.module').then(
-        (m) => m.DashboardModule
-      ),
-  },
 
-  {
-    path: 'users',
-    loadChildren: () =>
-      import('../app/Admin/pages/users/users.module').then(
-        (m) => m.UsersModule
-      ),
-  },
   {
     path: '',
     pathMatch: 'full',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminModule } from './Admin/pages/admin.module';
+import { PublicModule } from './Public/pages/public.module';
 
 
 
@@ -12,6 +14,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AdminModule,
+    PublicModule,
     AppRoutingModule
   ],
   providers: [],
